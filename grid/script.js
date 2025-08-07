d13a3039348c75f3c49ff9c51f53c07a47448599
ssu <iso99jsj@naver.com>
2025-08-07 22:01:28 +0900

    Delete grid directory

--- grid/script.js
+++
@@ -1,6 +0,0 @@
-const elem = document.querySelectorAll("nav>ul>li>label");
-elem.forEach((label)=>{
-    label.addEventListener("click",()=>{
-        document.getElementById("check-icon"). checked=false;
-    })
-});
